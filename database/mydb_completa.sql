13543d6916b000690dceba27ac24c1ad2cb79849
SirBeho <benjamin.tavarez.98@gmail.com>
2023-08-16 12:52:54 -0400

    database

--- database/mydb_completa.sql
+++ database/mydb_completa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 16-08-2023 a las 01:19:37
+-- Tiempo de generación: 16-08-2023 a las 16:48:13
 -- Versión del servidor: 8.0.31
 -- Versión de PHP: 8.2.0
 
@@ -362,13 +362,13 @@ CREATE TABLE IF NOT EXISTS `persona` (
   `sexo` varchar(45) NOT NULL,
   `doc_identidad` varchar(45) NOT NULL,
   `idDireccion` int NOT NULL,
-  `correo` int NOT NULL,
+  `idcorreo` int NOT NULL,
   `TipoPersona` int NOT NULL,
-  `telefono` int NOT NULL,
+  `idtelefono` int NOT NULL,
   PRIMARY KEY (`idPersona`),
   KEY `tipoPersoa_idx` (`TipoPersona`),
-  KEY `correo_idx` (`correo`),
-  KEY `telefono_idx` (`telefono`),
+  KEY `correo_idx` (`idcorreo`),
+  KEY `telefono_idx` (`idtelefono`),
   KEY `direccion_idx` (`idDireccion`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
@@ -376,7 +376,7 @@ CREATE TABLE IF NOT EXISTS `persona` (
 -- Volcado de datos para la tabla `persona`
 --
 
-INSERT INTO `persona` (`idPersona`, `nombre`, `apellido`, `sexo`, `doc_identidad`, `idDireccion`, `correo`, `TipoPersona`, `telefono`) VALUES
+INSERT INTO `persona` (`idPersona`, `nombre`, `apellido`, `sexo`, `doc_identidad`, `idDireccion`, `idcorreo`, `TipoPersona`, `idtelefono`) VALUES
 (1, 'Juan', 'Pérez', 'M', '001-0000000-0', 1, 1, 1, 1),
 (2, 'María', 'Gómez', 'F', '002-1111111-1', 2, 2, 1, 2),
 (3, 'Pedro', 'Ramírez', 'M', '003-2222222-2', 3, 3, 2, 3),
@@ -720,6 +720,7 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `NombreUser` varchar(45) NOT NULL,
   `PasswUser` varchar(45) NOT NULL,
   `idPersona` int NOT NULL,
+  `interno` tinyint(1) NOT NULL,
   PRIMARY KEY (`idUsuario`),
   KEY `persona_idx` (`idPersona`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
@@ -728,27 +729,27 @@ CREATE TABLE IF NOT EXISTS `usuario` (
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`idUsuario`, `NombreUser`, `PasswUser`, `idPersona`) VALUES
-(1, 'admin', 'admin', 1),
-(2, 'maria_gomez', 'password2', 2),
-(3, 'pedro_ramirez', 'password3', 3),
-(4, 'ana_santos', 'password4', 4),
-(5, 'carlos_gonzalez', 'password5', 5),
-(6, 'sofia_hernandez', 'password6', 6),
-(7, 'luis_lopez', 'password7', 7),
-(8, 'laura_torres', 'password8', 8),
-(9, 'jose_mendoza', 'password9', 9),
-(10, 'isabel_fernandez', 'password10', 10),
-(11, 'raul_jimenez', 'password11', 11),
-(12, 'marta_castillo', 'password12', 12),
-(13, 'jorge_cruz', 'password13', 13),
-(14, 'rosa_mendez', 'password14', 14),
-(15, 'francisco_garcia', 'password15', 15),
-(16, 'elena_reyes', 'password16', 16),
-(17, 'daniel_rojas', 'password17', 17),
-(18, 'gabriela_acosta', 'password18', 18),
-(19, 'mario_sanchez', 'password19', 19),
-(20, 'carmen_mora', 'password20', 20);
+INSERT INTO `usuario` (`idUsuario`, `NombreUser`, `PasswUser`, `idPersona`, `interno`) VALUES
+(1, 'admin', 'admin', 1, 1),
+(2, 'user', 'user', 2, 0),
+(3, 'pedro_ramirez', 'password3', 3, 1),
+(4, 'ana_santos', 'password4', 4, 1),
+(5, 'carlos_gonzalez', 'password5', 5, 1),
+(6, 'sofia_hernandez', 'password6', 6, 1),
+(7, 'luis_lopez', 'password7', 7, 1),
+(8, 'laura_torres', 'password8', 8, 1),
+(9, 'jose_mendoza', 'password9', 9, 1),
+(10, 'isabel_fernandez', 'password10', 10, 1),
+(11, 'raul_jimenez', 'password11', 11, 1),
+(12, 'marta_castillo', 'password12', 12, 1),
+(13, 'jorge_cruz', 'password13', 13, 1),
+(14, 'rosa_mendez', 'password14', 14, 1),
+(15, 'francisco_garcia', 'password15', 15, 1),
+(16, 'elena_reyes', 'password16', 16, 1),
+(17, 'daniel_rojas', 'password17', 17, 1),
+(18, 'gabriela_acosta', 'password18', 18, 1),
+(19, 'mario_sanchez', 'password19', 19, 1),
+(20, 'carmen_mora', 'password20', 20, 1);
 
 -- --------------------------------------------------------
 
@@ -878,6 +879,7 @@ CREATE TABLE IF NOT EXISTS `vehiculos_venta` (
   `year` varchar(45) NOT NULL,
   `vehiculo_modelo` int DEFAULT NULL,
   `vehiculo_Categoria` int NOT NULL,
+  `nuevo` tinyint(1) NOT NULL DEFAULT '1',
   `image` varchar(100) NOT NULL,
   PRIMARY KEY (`idVehiculos_Venta`),
   UNIQUE KEY `vehiculo_modelo_UNIQUE` (`vehiculo_modelo`),
@@ -888,27 +890,27 @@ CREATE TABLE IF NOT EXISTS `vehiculos_venta` (
 -- Volcado de datos para la tabla `vehiculos_venta`
 --
 
-INSERT INTO `vehiculos_venta` (`idVehiculos_Venta`, `precio`, `millage`, `fecha_adquisicion`, `year`, `vehiculo_modelo`, `vehiculo_Categoria`, `image`) VALUES
-(1, 25000, 20000, '2023-01-01', '2022', 1, 1, 'corolla.jpg'),
-(2, 28000, 15000, '2023-01-02', '2022', 2, 1, 'civic.jpg'),
-(3, 32000, 10000, '2023-01-03', '2022', 3, 2, 'f150.jpg'),
-(4, 35000, 12000, '2023-01-04', '2022', 4, 2, 'silverado.jpg'),
-(5, 26000, 18000, '2023-01-05', '2022', 5, 3, 'altima.jpg'),
-(6, 34000, 8000, '2023-01-06', '2022', 6, 1, '3series.jpg'),
-(7, 39000, 7000, '2023-01-07', '2022', 7, 1, 'cclass.jpg'),
-(8, 42000, 5000, '2023-01-08', '2022', 8, 2, 'a4.jpg'),
-(9, 28000, 12000, '2023-01-09', '2022', 9, 2, 'elantra.jpg'),
-(10, 31000, 9000, '2023-01-10', '2022', 10, 3, 'sorento.jpg'),
-(11, 28000, 17000, '2023-01-11', '2022', 11, 3, 'golf.jpg'),
-(12, 33000, 15000, '2023-01-12', '2022', 12, 1, 'cx5.jpg'),
-(13, 38000, 12000, '2023-01-13', '2022', 13, 1, 'outback.jpg'),
-(14, 39000, 18000, '2023-01-14', '2022', 14, 2, 'wrangler.jpg'),
-(15, 45000, 8000, '2023-01-15', '2022', 15, 2, 'rx.jpg'),
-(16, 32000, 20000, '2023-01-16', '2022', 16, 3, 'xc60.jpg'),
-(17, 49000, 7000, '2023-01-17', '2022', 17, 3, '488gtb.jpg'),
-(18, 52000, 6000, '2023-01-18', '2022', 18, 1, '911.jpg'),
-(19, 80000, 3000, '2023-01-19', '2022', 19, 1, 'models.jpg'),
-(20, 85000, 5000, '2023-01-20', '2022', 20, 2, 'rangerover.jpg');
+INSERT INTO `vehiculos_venta` (`idVehiculos_Venta`, `precio`, `millage`, `fecha_adquisicion`, `year`, `vehiculo_modelo`, `vehiculo_Categoria`, `nuevo`, `image`) VALUES
+(1, 25000, 20000, '2023-01-01', '2022', 1, 1, 1, 'corolla.jpg'),
+(2, 28000, 15000, '2023-01-02', '2022', 2, 1, 1, 'civic.jpg'),
+(3, 32000, 10000, '2023-01-03', '2022', 3, 2, 1, 'f150.jpg'),
+(4, 35000, 12000, '2023-01-04', '2022', 4, 2, 1, 'silverado.jpg'),
+(5, 26000, 18000, '2023-01-05', '2022', 5, 3, 1, 'altima.jpg'),
+(6, 34000, 8000, '2023-01-06', '2022', 6, 1, 1, '3series.jpg'),
+(7, 39000, 7000, '2023-01-07', '2022', 7, 1, 1, 'cclass.jpg'),
+(8, 42000, 5000, '2023-01-08', '2022', 8, 2, 1, 'a4.jpg'),
+(9, 28000, 12000, '2023-01-09', '2022', 9, 2, 1, 'elantra.jpg'),
+(10, 31000, 9000, '2023-01-10', '2022', 10, 3, 1, 'sorento.jpg'),
+(11, 28000, 17000, '2023-01-11', '2022', 11, 3, 1, 'golf.jpg'),
+(12, 33000, 15000, '2023-01-12', '2022', 12, 1, 1, 'cx5.jpg'),
+(13, 38000, 12000, '2023-01-13', '2022', 13, 1, 1, 'outback.jpg'),
+(14, 39000, 18000, '2023-01-14', '2022', 14, 2, 1, 'wrangler.jpg'),
+(15, 45000, 8000, '2023-01-15', '2022', 15, 2, 1, 'rx.jpg'),
+(16, 32000, 20000, '2023-01-16', '2022', 16, 3, 1, 'xc60.jpg'),
+(17, 49000, 7000, '2023-01-17', '2022', 17, 3, 1, '488gtb.jpg'),
+(18, 52000, 6000, '2023-01-18', '2022', 18, 1, 1, '911.jpg'),
+(19, 80000, 3000, '2023-01-19', '2022', 19, 1, 1, 'models.jpg'),
+(20, 85000, 5000, '2023-01-20', '2022', 20, 2, 1, 'rangerover.jpg');
 
 -- --------------------------------------------------------
 
@@ -1033,9 +1035,9 @@ ALTER TABLE `pagoscli`
 -- Filtros para la tabla `persona`
 --
 ALTER TABLE `persona`
-  ADD CONSTRAINT `correo` FOREIGN KEY (`correo`) REFERENCES `correos` (`idcorreos`),
+  ADD CONSTRAINT `correo` FOREIGN KEY (`idcorreo`) REFERENCES `correos` (`idcorreos`),
   ADD CONSTRAINT `direccion` FOREIGN KEY (`idDireccion`) REFERENCES `direccion` (`idDireccion`),
-  ADD CONSTRAINT `telefono` FOREIGN KEY (`telefono`) REFERENCES `telefono` (`idtelefono`),
+  ADD CONSTRAINT `telefono` FOREIGN KEY (`idtelefono`) REFERENCES `telefono` (`idtelefono`),
   ADD CONSTRAINT `tipoPersoa` FOREIGN KEY (`TipoPersona`) REFERENCES `tipopersona` (`idTipoPersona`);
 
 --
@@ -1082,6 +1084,12 @@ ALTER TABLE `vehiculos_modelos`
 ALTER TABLE `vehiculos_vendidos`
   ADD CONSTRAINT `cliente` FOREIGN KEY (`idCliente`) REFERENCES `cliente` (`idCliente`),
   ADD CONSTRAINT `vehiculoVenta` FOREIGN KEY (`idVehiculoVenta`) REFERENCES `vehiculos_venta` (`idVehiculos_Venta`);
+
+--
+-- Filtros para la tabla `vehiculos_venta`
+--
+ALTER TABLE `vehiculos_venta`
+  ADD CONSTRAINT `categoria` FOREIGN KEY (`vehiculo_Categoria`) REFERENCES `vehiculo_categoria` (`idVehiculo_Categoria`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
